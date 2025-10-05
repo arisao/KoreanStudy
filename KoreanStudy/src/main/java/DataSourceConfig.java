de46254a10a54ec7dcdda6b07370061d5ab7d889
arisaohtaarisaohtaarisaohta <arisaohta@localhost>
2025-10-05 22:07:56 +0900

    暗号化処理追加

---
+++ KoreanStudy/src/main/java/DataSourceConfig.java
@@ -0,0 +1,14 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
--- KoreanStudy/src/main/java/com/koreanstudy/KoreanStudyApplication.java
+++ KoreanStudy/src/main/java/com/koreanstudy/KoreanStudyApplication.java
@@ -1,13 +1,18 @@
 package com.koreanstudy;
-
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class KoreanStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KoreanStudyApplication.class, args);
+		System.out.println("JASYPT_ENCRYPTOR_PASSWORD=" + System.getenv("JASYPT_ENCRYPTOR_PASSWORD"));
+
 	}
 
 }
